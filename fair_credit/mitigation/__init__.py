7c201d07aa4d27a232b4529719bfaa9caf5ef9b7
Prishita Ghanathe <102364009+Prishita01@users.noreply.github.com>
2025-10-17 02:30:44 +0530

    Initialize mitigation module

---
+++ fair_credit/mitigation/__init__.py
@@ -0,0 +1,9 @@
+from .reweighing import ReweighingMitigator
+from .threshold_optimization import ThresholdOptimizer
+from .evaluator import MitigationEvaluator
+
+__all__ = [
+    "ReweighingMitigator",
+    "ThresholdOptimizer", 
+    "MitigationEvaluator"
+]
